7988a9ecb1ccad26121b82cf048a35b23a1b9625
Wanchen <wghong@bu.edu>
2023-08-28 01:05:07 -0700

    added models

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # Here begins the code for the neural network model
 
+# Load packages
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,6 +20,8 @@ from IPython.display import display
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # print(device)
 # print(torch.cuda.get_device_name(device))
+
+## Load and transform the data
 
 # Transform the data and labels here
 # The 224x224 images are processed with random horizontal flip, random rotation and normalization
@@ -47,6 +50,7 @@ trainloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, 
 dataset2 = torchvision.datasets.ImageFolder(root=dataset_dir + "test", transform = test_tfms)
 testloader = torch.utils.data.DataLoader(dataset2, batch_size=32, shuffle=False, num_workers=2)
 
+# Train and Test the Model
 def train_model(model, criterion, optimizer, scheduler, n_epochs=5):
 
     losses = []
@@ -101,6 +105,7 @@ def train_model(model, criterion, optimizer, scheduler, n_epochs=5):
     print('Finished Training')
     return model, losses, accuracies, test_accuracies
 
+# Evaluate the Model on training data
 def eval_model(model):
     correct = 0.0
     total = 0.0
@@ -122,3 +127,157 @@ def eval_model(model):
     print('Accuracy of the network on the test images: %d %%' % (
         test_acc))
     return test_acc
+
+## Tuning the Model - AlexNet
+
+# define parameters
+
+NUM_CAR_CLASSES = 196
+
+# use alexnet as the base model
+model_ft_an = models.alexnet(pretrained=True)
+
+# Freeze model parameters and define the FC layer to be attached to the model,
+# loss function and the optimizer.
+
+# put the model on the GPUs
+for param in model_ft_an.parameters():
+    param.require_grad = False
+
+# replace the last fc layer with an untrained one (requires grad)
+
+num_ftrs_an = model_ft_an.classifier[6].in_features
+model_ft_an.classifier[6] = nn.Linear(num_ftrs_an, NUM_CAR_CLASSES)
+
+model_ft_an = model_ft_an.to(device)
+
+# half precision model
+# model_ft_an = model_ft_an.half()
+
+# for layer in model_ft_an.modules():
+#     if isinstance(layer, nn.BatchNorm2d):
+#         layer.float()
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model_ft.parameters(), lr=0.01, momentum=0.9)
+
+lrscheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=3, threshold = 0.9)
+
+# model training
+model_ft_an, training_losses, training_accs, test_accs = train_model(model_ft_an, criterion, optimizer, lrscheduler, n_epochs=20)
+
+# plot the stats
+
+f, axarr = plt.subplots(2,2, figsize = (12, 8))
+axarr[0, 0].plot(training_losses)
+axarr[0, 0].set_title("Training loss, AlexNet")
+axarr[0, 1].plot(training_accs)
+axarr[0, 1].set_title("Training acc, AlexNet")
+axarr[1, 0].plot(test_accs)
+
+axarr[1, 0].set_title("Test acc, AlexNet")
+
+# Evaluate the model on single images
+
+# tie the class indices to their names
+
+def find_classes(dir):
+    classes = os.listdir(dir)
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    return classes, class_to_idx
+classes, c_to_idx = find_classes(dataset_dir + "train")
+
+# test the model on random images
+
+# switch the model to evaluation mode to make dropout and batch norm work in eval mode
+model_ft_an.eval()
+
+# transforms for the input image
+loader = transforms.Compose([transforms.Resize((400, 400)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+image = Image.open(dataset_dir+"test/Mercedes-Benz C-Class Sedan 2012/01977.jpg")
+image = loader(image).float()
+image = torch.autograd.Variable(image, requires_grad=True)
+image = image.unsqueeze(0)
+image = image.cuda()
+output = model_ft(image)
+conf, predicted = torch.max(output.data, 1)
+
+# get the class name of the prediction
+display(Image.open(dataset_dir+"test/Mercedes-Benz C-Class Sedan 2012/01977.jpg"))
+print(classes[predicted.item()], "confidence: ", conf.item())
+
+## Tuning the model - ResNet34
+
+# define parameters
+
+NUM_CAR_CLASSES = 196
+
+# use resnet as the base model
+model_ft_rn = models.resnet34(pretrained=True)
+
+# Freeze model parameters and define the FC layer to be attached to the model,
+# loss function and the optimizer.
+
+# put the model on the GPUs
+for param in model_ft_rn.parameters():
+    param.require_grad = False
+
+# replace the last fc layer with an untrained one (requires grad)
+
+# resnet34
+num_ftrs_rn = model_ft_rn.fc.in_features
+model_ft_rn.fc = nn.Linear(num_ftrs_rn, NUM_CAR_CLASSES)
+
+model_ft_rn = model_ft_rn.to(device)
+
+# half precision model
+# model_ft_rn = model_ft_rn.half()
+# for layer in model_ft_rn.modules():
+#     if isinstance(layer, nn.BatchNorm2d):
+#         layer.float()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model_ft_rn.parameters(), lr=0.01, momentum=0.9)
+
+lrscheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=3, threshold = 0.9)
+
+# model training
+
+model_ft_rn, training_losses, training_accs, test_accs = train_model(model_ft, criterion, optimizer, lrscheduler, n_epochs=20)
+
+# plot the stats
+
+f, axarr = plt.subplots(2,2, figsize = (12, 8))
+axarr[0, 0].plot(training_losses)
+axarr[0, 0].set_title("Training loss, ResNet34")
+axarr[0, 1].plot(training_accs)
+axarr[0, 1].set_title("Training acc, ResNet34")
+axarr[1, 0].plot(test_accs)
+
+axarr[1, 0].set_title("Test acc, ResNet34")
+
+# Evaluate the model on single images
+# test the model on random images
+
+# switch the model to evaluation mode to make dropout and batch norm work in eval mode
+model_ft_rn.eval()
+
+# transforms for the input image
+loader = transforms.Compose([transforms.Resize((400, 400)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+image = Image.open(dataset_dir+"test/Mercedes-Benz C-Class Sedan 2012/01977.jpg")
+image = loader(image).float()
+image = torch.autograd.Variable(image, requires_grad=True)
+image = image.unsqueeze(0)
+image = image.cuda()
+output = model_ft(image)
+conf, predicted = torch.max(output.data, 1)
+
+# get the class name of the prediction
+display(Image.open(dataset_dir+"test/Mercedes-Benz C-Class Sedan 2012/01977.jpg"))
+print(classes[predicted.item()], "confidence: ", conf.item())
