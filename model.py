b7146ec3148b9ce4452ad18976f6e49c35ac615f
Wanchen <wghong@bu.edu>
2023-08-27 19:25:09 -0700

    add train eval functions

--- model.py
+++ model.py
@@ -47,3 +47,78 @@ trainloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, 
 dataset2 = torchvision.datasets.ImageFolder(root=dataset_dir + "test", transform = test_tfms)
 testloader = torch.utils.data.DataLoader(dataset2, batch_size=32, shuffle=False, num_workers=2)
 
+def train_model(model, criterion, optimizer, scheduler, n_epochs=5):
+
+    losses = []
+    accuracies = []
+    test_accuracies = []
+
+    # set the model to train mode initially
+    model.train()
+    for epoch in tqdm.tqdm(range(n_epochs)):
+        since = time.time()
+        running_loss = 0.0
+        running_correct = 0.0
+        for i, data in enumerate(trainloader, 0):
+
+            # get the inputs and assign them to cuda
+            inputs, labels = data
+            #inputs = inputs.to(device).half() # half precision model to quickly check
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            # torch.cuda.amp.autocast() # for half precision model
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # accumulate loss & acc
+
+            running_loss += loss.item()
+            running_correct += (labels==predicted).sum().item()
+
+        epoch_duration = time.time() - since
+        epoch_loss = running_loss / len(trainloader)
+        epoch_acc = 100 / 32 * running_correct / len(trainloader)
+        print("Epoch %s, duration: %d s, loss: %.4f, acc: %.4f" % (epoch+1, epoch_duration, epoch_loss, epoch_acc))
+
+        losses.append(epoch_loss)
+        accuracies.append(epoch_acc)
+
+        # switch the model to eval mode to evaluate on test data
+        model.eval()
+        test_acc = eval_model(model)
+        test_accuracies.append(test_acc)
+
+        # re-set the model to train mode after validating
+        model.train()
+        scheduler.step(test_acc)
+        since = time.time()
+    print('Finished Training')
+    return model, losses, accuracies, test_accuracies
+
+def eval_model(model):
+    correct = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for i, data in enumerate(testloader, 0):
+            images, labels = data
+            #images = images.to(device).half() # half precision model
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    test_acc = 100.0 * correct / total
+    print('Accuracy of the network on the test images: %d %%' % (
+        test_acc))
+    return test_acc
