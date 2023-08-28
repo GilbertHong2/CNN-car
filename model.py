767aedd0b079430ef45bcdbcc66511b1302a8975
Wanchen <wghong@bu.edu>
2023-08-27 19:15:03 -0700

    added loading data

--- model.py
+++ model.py
@@ -1,2 +1,49 @@
 # Here begins the code for the neural network model
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import time
+import os
+import tqdm
+import PIL.Image as Image
+from IPython.display import display
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# print(device)
+# print(torch.cuda.get_device_name(device))
+
+# Transform the data and labels here
+# The 224x224 images are processed with random horizontal flip, random rotation and normalization
+
+dataset_dir = "stanford_car_dataset/car_data/car_data/"
+
+# data transformation, you can try different transformation/ data augmentation here
+# note: no data augmentation for test data
+
+width, height = 224, 224
+train_tfms = transforms.Compose([transforms.Resize((width, height)),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.RandomRotation(15),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])  # mean and std from imagenet dataset
+test_tfms = transforms.Compose([transforms.Resize((width, height)),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.RandomRotation(15),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+# create datasets
+dataset = torchvision.datasets.ImageFolder(root=dataset_dir + "train", transform = train_tfms)
+trainloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, num_workers=2)
+
+dataset2 = torchvision.datasets.ImageFolder(root=dataset_dir + "test", transform = test_tfms)
+testloader = torch.utils.data.DataLoader(dataset2, batch_size=32, shuffle=False, num_workers=2)
+
